a32fef022733c925bcf30b5557da51fac280007e
ilovetocode <54194229+ilovetocode2019@users.noreply.github.com>
2024-11-17 19:21:17 -0800

    Set registerType to autoUpdate for PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
     manifest: {
       name: "Thumbtacked",
       short_name: "Thumbtacked",
